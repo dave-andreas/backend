f7c62ed6295bf11c6993b1b75d1aeb4f47712509
dave-andreas <david.roynaldo.25@gmail.com>
2020-03-18 00:49:19 +0700

    18

--- controlers/usercontroler.js
+++ controlers/usercontroler.js
@@ -42,4 +42,72 @@ module.exports = {
             })
         })
     },
+    savecart:(req,res)=>{
+        console.log(req.body)
+        const {userid,modelid,bahanid,warna,bodysizeid,jumlah,harga} = req.body
+        const data = {
+            userid,modelid,bahanid,warna,bodysizeid,jumlah,harga,
+            orderid:0,
+            jadi:0
+        }
+        var sql = `insert into order_detil set ?`
+        db.query(sql,data,(err,result)=>{
+            if (err) return res.status(500).send(err)
+            return res.status(200).send('Successfully adding to cart')
+        })
+    },
+    // -----------------------------------------------------------
+    getboze:(req,res)=>{
+        const {id} = req.params
+        var sql = `select * from bodysize where userid=${id}`
+        db.query(sql,(err,result)=>{
+            if (err) return res.status(500).send(err)
+            return res.status(200).send(result)
+        })
+    },
+    addboze:(req,res)=>{
+        const data = req.body
+        var sql = `select into bodysize set ?`
+        db.query(sql,(err,result)=>{
+            if (err) return res.status(500).send(err)
+            sql =  `select * from bodysize where userid=${data.userid}`
+            db.query(sql,(err,result)=>{
+                if (err) return res.status(500).send(err)
+                return res.status(200).send(result)
+            })
+        })
+    },
+    delboze:(req,res)=>{
+        const {id,userid} = req.query
+        var sql = `delete from bodysize where id=${id}`
+        db.query(sql,(err,result)=>{
+            if (err) return res.status(500).send(err)
+            sql = `select * from bodysize where userid=${userid}`
+            db.query(sql,(err,result)=>{
+                if (err) return res.status(500).send(err)
+                return res.status(200).send(result)
+            })
+        })
+    },
+    editboze:(req,res)=>{
+        const {data} = req.body
+        var sql = `update bodysize set name='${data.name}', lingping=${data.lingping}, lingba=${data.lingba}, leda=${data.leda}, tule=${data.tule}, panmu=${data.panmu}, linggul=${data.linggul}, panpung=${data.panpung}, lepun=${data.lepun}, leba=${data.leba}, kele=${data.kele}, panle=${data.panle}, lule=${data.lule} where id=${data.id};`
+        db.query(sql,(err,result)=>{
+            if (err) return res.status(500).send(err)
+            sql = `select * from bodysize where userid=${data.userid}`
+            db.query(sql,(err,result)=>{
+                if (err) return res.status(500).send(err)
+                return res.status(200).send(result)
+            })
+        })
+    },
+    // ----------------------- coba ------------------------------
+    coba:(req,res)=>{
+
+        var sql = `update bahan_model set idbahan=6 where idmodel=1`
+        db.query(sql,(err,result)=>{
+            if (err) return res.status(500).send(err)
+            return res.status(200).send('Your order have been reported, please wait our admin for the confirmation. Thank you!')
+        })
+    }
 }
--- routers/userrouter.js
+++ routers/userrouter.js
@@ -5,5 +5,13 @@ const router = express.Router()
 
 router.get('/ordering',usercontroler.ordering)
 router.get('/confirm',usercontroler.confirm)
+router.post('/savecart',usercontroler.savecart)
+
+router.get('/getboze/:id',usercontroler.getboze)
+router.post('/addboze',usercontroler.addboze)
+router.put('/delboze',usercontroler.delboze)
+router.put('/editboze',usercontroler.editboze)
+
+router.put('/coba',usercontroler.coba)
 
 module.exports = router
