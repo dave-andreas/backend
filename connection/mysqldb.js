40b61576b4a5a1240cadfc1049f9d0fa5b5c95bf
dave-andreas <david.roynaldo.25@gmail.com>
2020-04-06 18:59:52 +0700

    1.1

--- connection/mysqldb.js
+++ connection/mysqldb.js
@@ -1,11 +1,28 @@
 const mysql = require('mysql')
 
-const db=mysql.createConnection({
-    host:'localhost',
-    user:'david',
-    password:'!2#)9*RoY',
-    database:'jahitin',
+// const db=mysql.createConnection({
+//     host:'localhost',
+//     user:'david',
+//     password:'!2#)9*RoY',
+//     database:'jahitin',
+//     port:'3306'
+// })
+
+const db = mysql.createConnection ({
+    host:'db4free.net',
+    user:'davroy',
+    password:'pew4955JHStU@Nz',
+    database:'deploy1st',
     port:'3306'
 })
+//693wxV#qEnu9nwTc
+
+// const db = mysql.createConnection ({
+//     host:'remotemysql.com',
+//     user:'CpHSLtzqVx',
+//     password:'erGLrDIToX',
+//     database:'CpHSLtzqVx',
+//     port:'3306'
+// })
 
 module.exports=db
