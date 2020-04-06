b41334126bcc3a17e0bb496ccef2a8b8a4c9e6ff
dave-andreas <david.roynaldo.25@gmail.com>
2020-04-06 19:09:34 +0700

    2

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const cors=require('cors')
 const bearerToken=require('express-bearer-token')
 const {authrouter,adminrouter,userrouter}=require('./routers')
 const schedule=require('node-schedule')
+const morgan = require('morgan')
 
 const PORT= process.env.PORT || 2020
 
+app.use(morgan('short'))
 app.use(cors())
 app.use(bearerToken())
 app.use(BodyParser.json())
